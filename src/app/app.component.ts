8cd8e05819100902a7c687576d1362151681dcf3
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-10-26 19:12:50 -0500

    add foundation js component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import { UserModel } from './user/user.model';
 import { UserService } from './user/user.service';
+
+//TODO
+declare let $;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  config= {
+export class AppComponent implements OnInit, AfterViewInit {
+  config = {
     apiKey: 'AIzaSyAJG85EJA1lBmYS31zcOqjSECZ2YSpWHxo',
     authDomain: 'strike3-31769.firebaseapp.com',
     databaseURL: 'https://strike3-31769.firebaseio.com/',
     storageBucket: 'strike3-31769'
   };
 
-  constructor(public userModel: UserModel, public userService: UserService) {}
+  constructor(public userModel: UserModel, public userService: UserService) {
+  }
 
   ngOnInit(): void {
     firebase.initializeApp(this.config);
     this.userModel.init();
+  }
+
+  //TODO does this have to be here?
+  ngAfterViewInit(): void {
+    $('body').foundation();
   }
 
   signIn() {
