dddc5090e19e709315fbd49bec3b52f3a6ba831a
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-11-07 20:16:47 -0600

    game view show boxes

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,7 +4,8 @@ import { UserModel } from '../user/user.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   config = {
