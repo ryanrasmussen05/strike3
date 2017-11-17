d6b441165a78f8e71cfbe2e906a09ce9d6d4b9e1
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-11-16 19:15:49 -0600

    remove stuff

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-
-exports.editPick = functions.firestore.document('picks/{pickId}').onCreate(event => {
-  var data = event.data.data();
-  data.time = new Date();
-
-  return event.data.ref.set(data, {merge: true});
-});
-
