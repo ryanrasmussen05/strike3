e2dbd169628b3fb43004e81669ceb9a8ce127277
Ryan <ryanrasmussen05@gmail.com>
2018-12-13 09:10:05 -0600

    remove unused code

--- src/app/components/pick/pick.component.ts
+++ src/app/components/pick/pick.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AppState } from '../../reducers';
 import { select, Store } from '@ngrx/store';
